6c828c9d05363c59cd5dd68bb1d7e1c269ece411
Maija Haka <maija.haka@live.fi>
2021-02-23 18:29:52 +0200

    build: initalise Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
