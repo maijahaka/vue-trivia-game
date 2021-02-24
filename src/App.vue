ff6e89ef0a78e8f60b477e1e821521e496600a4c
Maija Haka <maija.haka@live.fi>
2021-02-24 18:07:37 +0200

    refactor: rename Header component to AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Header title="Vue Trivia Game"/>
+    <app-header title="Vue Trivia Game"/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    AppHeader
   }
 }
 </script>
