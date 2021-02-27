fd8ef5cce1bf728e0f26334f79c6f0259cc46339
LLehtola <56760014+LLehtola@users.noreply.github.com>
2021-02-27 12:15:37 +0200

    Feat: Added StartScreenView and ResultScreenView and routing

---
+++ src/views/ResultScreenView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  <!--Redirect button to questions-->
+  <button v-on:click="restartgame">Re-start game</button>
+
+  </div>
+</template>
+
+<script>
+var restarter = new Vue({
+  el: '#restarter',
+  data: {
+    name: 'Vue.js'
+  },
+
+  methods: {
+    restartgame: function (event) {
+
+      // redirects to QuestionView when the button is clicked
+      this.$router.push('/views/StartScreenView.vue'); 
+      }
+    }
+})
+restarter.restartgame()
+</script>
+
+<!-- CSS styling - complete unless specified elsewhere -->
+<style>
+
+</style>
---
+++ src/views/StartScreenView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+    <h1>Geography Trivia Game</h1>
+
+    <p>
+      How well do you know your geography? 
+      This quiz will put your knowledge to the test!
+    </p>
+
+  <!--Redirect button to questions-->
+  <button v-on:click="startgame">Start game</button>
+
+  </div>
+</template>
+
+<script>
+var starter = new Vue({
+  el: '#starter',
+  data: {
+    name: 'Vue.js'
+  },
+
+  methods: {
+    startgame: function (event) {
+
+      // redirects to QuestionView when the button is clicked
+      this.$router.push('/views/QuestionView.vue'); 
+      }
+    }
+})
+starter.startgame()
+</script>
+
+<!-- CSS styling - complete unless specified elsewhere -->
+<style>
+
+</style>
