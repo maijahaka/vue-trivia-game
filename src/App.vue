35b88ca2d42686e159f198ebf19eea99a29b28be
Maija Haka <maija.haka@live.fi>
2021-02-27 21:11:47 +0200

    refactor: change router to abstract mode to hide URL paths and make the app return to start screen if page is reloaded

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ export default {
   name: 'App',
   components: {
     AppHeader
+  },
+  mounted() {
+    this.$router.replace('/')
   }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -21,6 +21,8 @@ const routes = [
     }
 ]
 
-const router = new VueRouter({ routes })
+const router = new VueRouter({ 
+    mode: 'abstract',
+    routes })
 
 export default router
